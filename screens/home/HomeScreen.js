0319e1b187a62f9994f93c629003527d0327e263
garcia64 <52061588+voltra40@users.noreply.github.com>
2020-09-26 00:23:27 -0400

    created leader board component on home screen with scrolling feature

--- screens/home/HomeScreen.js
+++ screens/home/HomeScreen.js
@@ -1,16 +1,66 @@
-import * as React from 'react';
-import {Text, View} from "react-native";
-
+import * as React from "react";
+import { StyleSheet, Text, View, SafeAreaView, ScrollView } from "react-native";
 
 export default class HomeScreen extends React.Component {
+  render() {
+    return (
+      <SafeAreaView style={[styles.container]}>
+        <View>
+          <View style={{ backgroundColor: "grey" }}>
+            <Text style={{ fontSize: 48, textAlign: "center" }}>
+              Leaderboard
+            </Text>
+          </View>
+          <ScrollView style={{ backgroundColor: "white" }}>
+            <Text style={{ fontSize: 16 }}>username</Text>
+            <Text style={{ fontSize: 16 }}>username</Text>
+            <Text style={{ fontSize: 16 }}>username</Text>
+            <Text style={{ fontSize: 16 }}>username</Text>
+            <Text style={{ fontSize: 16 }}>username</Text>
+            <Text style={{ fontSize: 16 }}>username</Text>
+            <Text style={{ fontSize: 16 }}>username</Text>
+            <Text style={{ fontSize: 16 }}>username</Text>
+            <Text style={{ fontSize: 16 }}>username</Text>
+            <Text style={{ fontSize: 16 }}>username</Text>
+            <Text style={{ fontSize: 16 }}>username</Text>
+            <Text style={{ fontSize: 16 }}>username</Text>
+            <Text style={{ fontSize: 16 }}>username</Text>
+            <Text style={{ fontSize: 16 }}>username</Text>
+            <Text style={{ fontSize: 16 }}>username</Text>
+            <Text style={{ fontSize: 16 }}>username</Text>
+            <Text style={{ fontSize: 16 }}>username</Text>
+            <Text style={{ fontSize: 16 }}>username</Text>
+            <Text style={{ fontSize: 16 }}>username</Text>
+            <Text style={{ fontSize: 16 }}>username</Text>
+            <Text style={{ fontSize: 16 }}>username</Text>
+            <Text style={{ fontSize: 16 }}>username</Text>
+            <Text style={{ fontSize: 16 }}>username</Text>
+            <Text style={{ fontSize: 16 }}>username</Text>
+            <Text style={{ fontSize: 16 }}>username</Text>
+            <Text style={{ fontSize: 16 }}>username</Text>
+            <Text style={{ fontSize: 16 }}>username</Text>
+            <Text style={{ fontSize: 16 }}>username</Text>
+            <Text style={{ fontSize: 16 }}>username</Text>
+            <Text style={{ fontSize: 16 }}>username</Text>
+            <Text style={{ fontSize: 16 }}>username</Text>
+            <Text style={{ fontSize: 16 }}>username</Text>
+            <Text style={{ fontSize: 16 }}>username</Text>
+            <Text style={{ fontSize: 16 }}>username</Text>
+            <Text style={{ fontSize: 16 }}>username</Text>
+          </ScrollView>
+        </View>
+      </SafeAreaView>
+    );
+  }
+}
 
-    render() {
-        return(
-            <View>
-                <Text> This is the Home Screen!</Text>
-            </View>
-        );
-    }
-
-
-};
+const styles = StyleSheet.create({
+  container: {
+    marginLeft: "5%",
+    marginRight: "5%",
+    marginTop: "80%",
+    marginBottom: "30%",
+    justifyContent: "center",
+    alignContent: "center",
+  },
+});
