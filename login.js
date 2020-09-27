fadd50cddabdeae0039afe6583ec7fecf4fc19e2
Youth-li <johnbrag@buffalo.edu>
2020-09-27 16:40:20 -0400

    made a more logic layout with headers and footers and created textbox for entering passwd&usrname. Also created sign in button.

--- login.js
+++ login.js
@@ -1,1 +1,78 @@
 
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Button, Text, View, TouchableOpacity } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      
+      <StatusBar style="auto" />
+      <View style={styles.header}>
+        <Text style={styles.welcomeWords}>Welcome! Happy Trading Games, and may the odds be ever in your favor. </Text>
+      </View>
+      <View style={styles.footer}>
+        <Text style={styles.footerWords}>Username</Text>
+        <TextInput 
+                    placeholder="Your Username"
+                    style={styles.textInput}
+                    autoCapitalize="none"
+                    
+                />
+      </View>
+    </View>
+    
+   
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  button: {
+    alignItems: 'center',
+    marginTop: 50
+},
+signIn: {
+    width: '100%',
+    height: 50,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 10
+},
+header: {
+  flex: 1,
+  justifyContent: 'flex-end',
+  paddingHorizontal: 20,
+  paddingBottom: 50
+},
+welcomeWords: {
+  color: '#403f42',
+  fontWeight: 'bold',
+  fontSize: 25
+},
+footer: {
+  flex: 4,
+  backgroundColor: '#889b73',
+  borderTopLeftRadius: 30,
+  borderTopRightRadius: 30,
+  paddingHorizontal: 20,
+  paddingVertical: 30
+},
+footerWords: {
+  color: '#05375a',
+  fontSize: 20,
+  textAlign: 'left'
+},
+textInput: {
+    flex: 1,
+    marginTop: 0,
+    paddingLeft: 10,
+    color: '#05375a',
+},
+
+});
