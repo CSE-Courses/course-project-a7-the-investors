e7509571bb3a0272bd0ea35dcf1aee657a9ff9b9
Youth-li <johnbrag@buffalo.edu>
2020-09-27 17:43:34 -0400

    just added a register button so that if you don't have an account already you can make one.

--- login.js
+++ login.js
@@ -1,10 +1,12 @@
-
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Button, Text, View, TouchableOpacity } from 'react-native';
+import { StyleSheet, Button, Text, View, TouchableOpacity, Dimensions, TextInput, Alert} from 'react-native';
+const height = Dimensions.get('window').height*0.75;
+const widthfoot = Dimensions.get('window').width;
 
 export default function App() {
   return (
+    
     <View style={styles.container}>
       
       <StatusBar style="auto" />
@@ -14,11 +16,32 @@ export default function App() {
       <View style={styles.footer}>
         <Text style={styles.footerWords}>Username</Text>
         <TextInput 
-                    placeholder="Your Username"
+                    placeholder="Your Username/Email"
+                    style={styles.textInput}
+                    autoCapitalize="none"
+                />
+        <Text style={styles.footerWords}>Password</Text>
+        <TextInput 
+                    placeholder="Your top secret Password"
                     style={styles.textInput}
                     autoCapitalize="none"
                     
-                />
+                /><TouchableOpacity onPress={() => Alert.alert('Simple Button pressed')}
+                style={styles.buttonForgot}>
+                  <Text style={styles.forgotPass}>Forgot Password?</Text>
+       </TouchableOpacity>
+        
+        <TouchableOpacity onPress={() => Alert.alert('Simple Button pressed')}
+           style={styles.buttonSign}>
+          <Text style={styles.buttonWords}>Sign In</Text>
+  </TouchableOpacity>
+
+  <Text style={{color: '#403f42', fontWeight: 'bold', fontSize: 25,alignItems: 'center', paddingLeft:widthfoot-(widthfoot/3), paddingTop:10}}>Or</Text>
+
+  <TouchableOpacity onPress={() => Alert.alert('Simple Button pressed')}
+           style={styles.buttonSign}>
+          <Text style={styles.buttonWords}>Register</Text>
+  </TouchableOpacity>
       </View>
     </View>
     
@@ -33,27 +56,20 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-  button: {
-    alignItems: 'center',
-    marginTop: 50
-},
-signIn: {
-    width: '100%',
-    height: 50,
-    justifyContent: 'center',
-    alignItems: 'center',
-    borderRadius: 10
-},
+  
+
 header: {
   flex: 1,
   justifyContent: 'flex-end',
   paddingHorizontal: 20,
   paddingBottom: 50
 },
+
 welcomeWords: {
   color: '#403f42',
   fontWeight: 'bold',
-  fontSize: 25
+  fontSize: 25,
+  alignSelf: "center",
 },
 footer: {
   flex: 4,
@@ -61,18 +77,59 @@ footer: {
   borderTopLeftRadius: 30,
   borderTopRightRadius: 30,
   paddingHorizontal: 20,
+  width:widthfoot,
   paddingVertical: 30
+  
 },
 footerWords: {
+  paddingTop: 20,
+  paddingBottom: 10,
   color: '#05375a',
   fontSize: 20,
   textAlign: 'left'
 },
 textInput: {
-    flex: 1,
-    marginTop: 0,
-    paddingLeft: 10,
-    color: '#05375a',
+  height: 40,
+  borderColor: '#EAFAF1',
+  borderWidth: 3,
+  marginBottom: 0,
+  color: '#05375a',
+  
 },
+forgotPass:{
+  color: '#05375a',
+  fontSize: 10,
+  textAlign: 'left',
+  paddingTop: 10,
+  paddingBottom: 10,
+  paddingLeft:6
+},
+
+buttonSign: {
+  elevation: 8,
+  backgroundColor: "#fff",
+  borderRadius: 10,
+  paddingVertical: 12,
+  paddingHorizontal: 12,
+  marginLeft: widthfoot/2,
+  marginTop: 20,
+  
+
+
+},
+buttonForgot:{
+  backgroundColor: "#889b73",
+  borderRadius: 3,
+  marginRight: widthfoot/2,
+  
+
+},
+buttonWords: {
+  fontSize: 18,
+  color: "#008000",
+  fontWeight: "bold",
+  alignSelf: "center",
+  textTransform: "uppercase"
+}
 
 });
