e382777650a21f75dc27f3266976ae421a599d1c
Michelle <mwong44@buffalo.edu>
2020-09-27 17:04:32 -0400

    Added a registration screen with username, email, and password. The buttons currently do not lead anywhere nor do they have any functionality yet.

---
+++ screens/search/SearchScreen.js
@@ -0,0 +1,17 @@
+import * as React from 'react';
+import {Text, View} from "react-native";
+import {Component} from "react";
+
+export default class SearchScreen extends Component{
+
+
+    render(){
+        return(
+            <View>
+                <Text> This is the Search Screen!</Text>
+            </View>
+        );
+    };
+
+
+};
