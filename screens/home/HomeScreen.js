81f00dad50f4ddb5f9dfde588b41c2fee455efa1
garcia64 <52061588+voltra40@users.noreply.github.com>
2020-09-28 17:10:33 -0400

    Increased organization by adding back HomeScreen Component, Homescreen now renders the LeaderBoard

---
+++ screens/home/HomeScreen.js
@@ -0,0 +1,9 @@
+import * as React from "react";
+import { Text, View } from "react-native";
+import LeaderBoard from "./LeaderBoard.js";
+
+export default class HomeScreen extends React.Component {
+  render() {
+    return <LeaderBoard />;
+  }
+}
