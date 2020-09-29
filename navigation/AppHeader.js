5fe52bf53893a1928c066bf8919c0091848df4c5
jemajews <36274776+jemajews@users.noreply.github.com>
2020-09-29 10:20:18 -0400

    Added wireframe of portfolio screen that will display what stocks you own

---
+++ navigation/AppHeader.js
@@ -0,0 +1,29 @@
+import {Appbar,Text} from "react-native-paper";
+import * as React from "react";
+import {Component} from "react";
+
+
+export default class AppHeader extends Component {
+
+
+    render() {
+
+        return (
+
+            <Appbar.Header style={style.appBar}>
+                <Appbar.Content
+                    title={<Text>Global</Text>}
+                    style={{ alignItems: 'center' }}
+                />
+            </Appbar.Header>
+        )
+            ;
+
+    }
+};
+
+const style = {
+    appBar: {
+        backgroundColor: 'tomato'
+    }
+}
---
+++ screens/portfolio/PortfolioStockRow.js
@@ -0,0 +1,46 @@
+import * as React from 'react';
+import {Text, View} from "react-native";
+import {Component} from "react";
+
+export default class PortfolioStockRow extends Component {
+
+    render() {
+        return (
+            <View style={styles.container}>
+                <Text style={styles.textStyling}> Tickr </Text>
+                <Text style={styles.textStyling}> -2.6% </Text>
+                <Text style={styles.textStyling}> 53 </Text>
+                <Text style={styles.textStyling}> $3.32 </Text>
+            </View>
+        );
+    }
+
+
+}
+
+const styles = {
+    container: {
+        flex: 1,
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        borderBottomWidth: 1,
+        borderColor: 'black',
+        width: '95%',
+        alignSelf: 'center',
+        height: 45,
+        marginTop: 8
+    },
+    textStyling: {
+        fontSize: 20
+    }
+
+};
+
+
+
+
+
+
+
+
+
