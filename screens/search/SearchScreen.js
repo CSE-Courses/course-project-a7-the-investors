1accda3f3f758a849d9993df2a7f4e7e0ec3527b
garcia64 <52061588+voltra40@users.noreply.github.com>
2020-09-28 22:41:06 -0400

    updating expo client so app can run

--- screens/search/SearchScreen.js
+++ screens/search/SearchScreen.js
@@ -1,58 +1,42 @@
-import * as React from 'react';
-import {Dimensions, Text, View} from "react-native";
-import {Component} from "react";
+import * as React from "react";
+import { Dimensions, Text, View, TextInput } from "react-native";
+import { Component } from "react";
 import StockBoard from "./StockBoard";
 import AppHeader from "../../navigation/AppHeader";
-import TextInput from "react-native-paper/src/components/TextInput/TextInput";
 import StockSearch from "./StockSearch";
 
 export default class SearchScreen extends Component {
-
-
-    render() {
-
-        return (
-
-            <View style={styles.container} >
-
-                <AppHeader/>
-                <StockSearch/>
-                <View style={styles.content}>
-                    <StockBoard/>
-                </View>
-
-
-            </View>
-
-        );
-    };
-
-
-};
+  render() {
+    return (
+      <View style={styles.container}>
+        <AppHeader />
+        <StockSearch />
+        <View style={styles.content}>
+          <StockBoard />
+        </View>
+      </View>
+    );
+  }
+}
 
 const styles = {
-    container: {
-        flex: 1,
-        flexDirection: 'column',
-
-
-    },
-    content: {
-        flex: 1,
-        flexDirection: 'column',
-        alignItems: 'center',
-    },
-    cashContainer: {
-        flex: 1,
-        flexDirection: 'column',
-        justifyContent: 'center',
-        alignContent: 'center',
-    },
-    cashHeader: {
-        flex: 1,
-        flexDirection: 'row',
-
-    }
-
-
+  container: {
+    flex: 1,
+    flexDirection: "column",
+  },
+  content: {
+    flex: 1,
+    flexDirection: "column",
+    alignItems: "center",
+  },
+  cashContainer: {
+    flex: 1,
+    flexDirection: "column",
+    justifyContent: "center",
+    alignContent: "center",
+  },
+  cashHeader: {
+    flex: 1,
+    flexDirection: "row",
+  },
 };
