4087a878b977b5fe52124ac9798fd247cae17a31
Chris Burton <cjburton@buffalo.edu>
2020-09-28 21:37:10 -0400

    Set up initial design for the profile screen.

--- screens/profile/ProfileScreen.js
+++ screens/profile/ProfileScreen.js
@@ -1,16 +1,32 @@
-import * as React from 'react';
-import {Text, View} from "react-native";
-
+import * as React from "react";
+import { Image, StyleSheet, Text, View } from "react-native";
 
 export default class ProfileScreen extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Image style={styles.image} source={require("./profile-icon.png")} />
+        <Text style={styles.text}>{"\n"}Username: ExampleName</Text>
+        <Text style={styles.text}>{"\n"}Email: user@example.com</Text>
+        <Text style={styles.text}>{"\n"}Member since 9/28/2020</Text>
+      </View>
+    );
+  }
+}
 
-    render() {
-        return(
-            <View>
-                <Text> This is the Profile Screen!</Text>
-            </View>
-        );
-    }
-
-
-};
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#00bbee",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  text: {
+    fontSize: 17,
+    color: "#000",
+  },
+  image: {
+    width: 150,
+    height: 150,
+  },
+});
