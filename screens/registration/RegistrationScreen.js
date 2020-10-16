39dc64a46fa12de63af5191ac87fce93fe7aeeba
jemajews <36274776+jemajews@users.noreply.github.com>
2020-10-16 13:34:25 -0400

    Added very basic functionality to registration with no checks. Task #8

--- screens/registration/RegistrationScreen.js
+++ screens/registration/RegistrationScreen.js
@@ -1,134 +1,168 @@
 import * as React from "react";
+import {AsyncStorage} from 'react-native';
+import Parse from 'parse/react-native.js';
 import {
-  Text,
-  View,
-  StyleSheet,
-  Button,
-  SafeAreaView,
-  Image,
+    Text,
+    View,
+    StyleSheet,
+    Button,
+    SafeAreaView,
+    Image,
 } from "react-native";
-import { Component } from "react";
-import { TextInput, TouchableOpacity } from "react-native-gesture-handler";
+import {Component} from "react";
+import {TextInput, TouchableOpacity} from "react-native-gesture-handler";
 
-export default class SearchScreen extends Component {
-  /*const [data, setData] = React.useState({
-        username: '',
-        email: '',
-        password: '',
-    });*/
+export default class RegistrationScreen extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            username: '',
+            email: '',
+            password: '',
+            confirmPassword: ''
 
-  render() {
-    return (
-      <SafeAreaView style={styles.container}>
-        <View style={styles.imageContainer}>
-          <Image
-            style={styles.image}
-            source={require("../../assets/stockPhoto.jpg")}
-          ></Image>
-        </View>
-        <TouchableOpacity style={styles.touchableButton}>
-          <Text style={styles.touchableText}> Sign up with Google </Text>
-          {/*onPress={()=> go to google and get info*/}
-        </TouchableOpacity>
+        }
+    }
 
-        <Text style={styles.setInfo}>Username</Text>
-        <TextInput
-          style={styles.input}
-          placeholder="Your username"
-          autoCapitalize="none"
-          //onChangeText={(val) => setUsername(val)}
-        />
+    /*const [data, setData] = React.useState({
+          username: '',
+          email: '',
+          password: '',
+      });*/
 
-        <Text style={styles.setInfo}>Email</Text>
-        <TextInput
-          style={styles.input}
-          placeholder="Your email"
-          autoCapitalize="none"
-          //onChangeText={(val) => setEmail(val)}
-        />
+    createAccount() {
+        Parse.setAsyncStorage(AsyncStorage);
+        Parse.initialize(
+            '9FkKTALI6H9nVAMUlemUOuSmfGml9pbJ5ais2Cbu', // This is your Application ID
+            'TRfJthSV8RSf85ShAHlrr4p2WfSGwqbv7LXYgEhP' // This is your Javascript key
+        );        Parse.serverURL = 'https://parseapi.back4app.com/'
+        const user = new Parse.User()
+       // const user = new User();
 
-        <Text style={styles.setInfo}>Password</Text>
-        <TextInput
-          style={styles.input}
-          secureTextEntry={true}
-          placeholder="Your password"
-          autoCapitalize="none"
-          //onChangeText={(val) => setPasswordAttempt1(val)}
-        />
+        user.set("username", this.state.username);
+        user.set("email", this.state.email);
+        user.set("password", this.state.password);
+        user.signUp().then((user) => {
+            if (typeof document !== 'undefined') document.write(`User signed up: ${JSON.stringify(user)}`);
+            console.log('User signed up', user);
+        }).catch(error => {
+            if (typeof document !== 'undefined') document.write(`Error while signing up user: ${JSON.stringify(error)}`);
+            console.error('Error while signing up user', error);
+        });
+    }
 
-        <Text style={styles.setInfo}>Confirm Password</Text>
-        <TextInput
-          style={styles.input}
-          secureTextEntry={true}
-          placeholder="Your password"
-          autoCapitalize="none"
-          //onChangeText={(val) => setPasswordAttempt2(val)}
-        />
+    render() {
 
-        <TouchableOpacity style={styles.touchableButton}>
-          <Text style={styles.touchableText}> Sign Up </Text>
-          {/*onPress={()=> add to database and go to home?*/}
-        </TouchableOpacity>
+        return (
+            <SafeAreaView style={styles.container}>
+                <View style={styles.imageContainer}>
+                    <Image
+                        style={styles.image}
+                        source={require("../../assets/stockPhoto.jpg")}
+                    />
+                </View>
+                <TouchableOpacity style={styles.touchableButton} onPress={() => this.createAccount()}>
+                    <Text style={styles.touchableText}> Sign up with Google </Text>
+                </TouchableOpacity>
 
-        <TouchableOpacity style={styles.touchable}>
-          <Text style={styles.loginText}> Already signed up? Login </Text>
-          {/*onPress={()=> navigation.navigate('Login')}*/}
-        </TouchableOpacity>
-      </SafeAreaView>
-    );
-  }
+                <Text style={styles.setInfo}>Username</Text>
+                <TextInput
+                    style={styles.input}
+                    placeholder="Your username"
+                    autoCapitalize="none"
+                    onChangeText={(text) => this.setState({username: text})}
+                />
+
+                <Text style={styles.setInfo}>Email</Text>
+                <TextInput
+                    style={styles.input}
+                    placeholder="Your email"
+                    autoCapitalize="none"
+                    onChangeText={(text) => this.setState({email: text})}
+                />
+
+                <Text style={styles.setInfo}>Password</Text>
+                <TextInput
+                    style={styles.input}
+                    secureTextEntry={true}
+                    placeholder="Your password"
+                    autoCapitalize="none"
+                    onChangeText={(text) => this.setState({password: text})}
+                />
+
+                <Text style={styles.setInfo}>Confirm Password</Text>
+                <TextInput
+                    style={styles.input}
+                    secureTextEntry={true}
+                    placeholder="Your password"
+                    autoCapitalize="none"
+                    onChangeText={(text) => this.setState({confirmPassword: text})}
+                />
+
+                <TouchableOpacity style={styles.touchableButton}>
+                    <Text style={styles.touchableText}> Sign Up </Text>
+                    {/*onPress={()=> add to database and go to home?*/}
+                </TouchableOpacity>
+
+                <TouchableOpacity style={styles.touchable}>
+                    <Text style={styles.loginText}> Already signed up? Login </Text>
+                    {/*onPress={()=> navigation.navigate('Login')}*/}
+                </TouchableOpacity>
+            </SafeAreaView>
+        );
+    }
 }
 styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-  },
-  imageContainer: {
-    alignItems: "center",
-    paddingTop: 30,
-    margin: 10,
-  },
-  image: {
-    width: 123,
-    height: 81,
-  },
-  input: {
-    borderWidth: 1,
-    borderColor: "#777",
-    padding: 10,
-    marginHorizontal: 20,
-    borderRadius: 5,
-  },
-  login: {
-    justifyContent: "center",
-    alignItems: "center",
-  },
-  loginText: {
-    fontSize: 14,
-  },
-  setInfo: {
-    backgroundColor: "#fff",
-    marginHorizontal: 20,
-    marginTop: 10,
-    marginBottom: 5,
-  },
-  touchable: {
-    justifyContent: "center",
-    alignItems: "center",
-    color: "#B95C24",
-    padding: 10,
-  },
-  touchableButton: {
-    justifyContent: "center",
-    alignItems: "center",
-    backgroundColor: "#B95C24",
-    padding: 10,
-    marginHorizontal: 30,
-    marginVertical: 10,
-    borderRadius: 15,
-  },
-  touchableText: {
-    fontWeight: "bold",
-    fontSize: 16,
-  },
+    container: {
+        flex: 1,
+        backgroundColor: "#fff",
+    },
+    imageContainer: {
+        alignItems: "center",
+        paddingTop: 30,
+        margin: 10,
+    },
+    image: {
+        width: 123,
+        height: 81,
+    },
+    input: {
+        borderWidth: 1,
+        borderColor: "#777",
+        padding: 10,
+        marginHorizontal: 20,
+        borderRadius: 5,
+    },
+    login: {
+        justifyContent: "center",
+        alignItems: "center",
+    },
+    loginText: {
+        fontSize: 14,
+    },
+    setInfo: {
+        backgroundColor: "#fff",
+        marginHorizontal: 20,
+        marginTop: 10,
+        marginBottom: 5,
+    },
+    touchable: {
+        justifyContent: "center",
+        alignItems: "center",
+        color: "#B95C24",
+        padding: 10,
+    },
+    touchableButton: {
+        justifyContent: "center",
+        alignItems: "center",
+        backgroundColor: "#B95C24",
+        padding: 10,
+        marginHorizontal: 30,
+        marginVertical: 10,
+        borderRadius: 15,
+    },
+    touchableText: {
+        fontWeight: "bold",
+        fontSize: 16,
+    },
 });
