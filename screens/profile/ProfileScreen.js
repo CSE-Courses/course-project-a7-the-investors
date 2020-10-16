a15e5327346eefcd1b6514830ac5db4729e6a8fc
Chris Burton <cjburton@buffalo.edu>
2020-10-16 00:14:59 -0400

    Open camera roll when tapping on profile: Task #13

--- screens/profile/ProfileScreen.js
+++ screens/profile/ProfileScreen.js
@@ -1,23 +1,35 @@
 import * as React from "react";
-import { Image, StyleSheet, Text, View } from "react-native";
+import { Image, StyleSheet, TouchableOpacity, Text, View, Button, Platform, PermissionsAndroid } from "react-native";
+import * as ImagePicker from 'expo-image-picker';
 
 export default class ProfileScreen extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Image style={styles.image} source={require("./profile-icon.png")} />
+        <TouchableOpacity onPress={profilePicPress}>
+          <Image style={styles.image} source={require("./profile-icon.png")} />
+        </TouchableOpacity>
         <Text style={styles.text}>{"\n"}Username: ExampleName</Text>
         <Text style={styles.text}>{"\n"}Email: user@example.com</Text>
         <Text style={styles.text}>{"\n"}Member since 9/28/2020</Text>
+        <Text/>
+        <Text/>
+        <Text/>
+        <Text/>
+        <Text/>
+        <Button
+          title = "Delete Account"
+          color = "ff00ff"
+          onPress = {() => console.log("Pressed")}
+        />
       </View>
     );
   }
-}
-
+};
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#00bbee",
+    backgroundColor: "tomato",
     alignItems: "center",
     justifyContent: "center",
   },
@@ -30,3 +42,17 @@ const styles = StyleSheet.create({
     height: 150,
   },
 });
+
+const profilePicPress = async () => {
+  let pic = await ImagePicker.launchImageLibraryAsync({
+    mediaTypes: ImagePicker.MediaTypeOptions.Images,
+    allowsMultipleSelection: false,
+    aspect: [1,1],
+    allowsEditing: false,
+    exif: false,
+  });
+};
+
+
+
+
