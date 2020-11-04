3d50b65cd82062e68b385929adae6eb9d4dd4fb0
garcia64 <52061588+voltra40@users.noreply.github.com>
2020-11-03 23:29:54 -0500

    Created functionality to list all current users #10

--- screens/home/LeaderBoard.js
+++ screens/home/LeaderBoard.js
@@ -1,8 +1,62 @@
 import * as React from "react";
 import { StyleSheet, Text, View, SafeAreaView, ScrollView } from "react-native";
 import LeaderBoardRow from "./LeaderBoardRow.js";
+import Parse from "parse/react-native.js";
+import { AsyncStorage } from "react-native";
 
 export default class Leaderboard extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      place: "",
+      username: "",
+      cash: "",
+      users: [],
+    };
+  }
+
+  async componentDidMount() {
+    await this.loadLeaderBoard();
+  }
+
+  async loadLeaderBoard() {
+    Parse.setAsyncStorage(AsyncStorage);
+    Parse.serverURL = "https://parseapi.back4app.com";
+    Parse.initialize(
+      "DQkWjHzOqleUvvD7H4seMLVzihUkKAFvxmjXzEAz",
+      "97TLDTbw7uSO8KL3jcOIAUpK500K02bv7440VqV4"
+    );
+    //need this key to fetch all users
+    Parse.masterKey = 'kZuJip8wEQ3vxC0OUMFCV0JTk52jZbihe17V2bH9';
+    const query = new Parse.Query("User");
+
+    //temporary array
+    const tempArray = [];
+
+    // Returns unique emails
+    query.distinct("username").then((results) => {
+      if (typeof document !== "undefined")
+        document.write(`Unique usernames: ${JSON.stringify(results)}`);
+      console.log(`Unique usernames: ${JSON.stringify(results)}`);
+      
+      //push all users to temporary array
+      for(var i = 0; i < results.length ; i++){
+        tempArray.push(results[i]);
+      }
+
+      //populate users with... users...
+      this.setState({users: tempArray});
+
+    })
+    .catch((error) => {
+      if (typeof document !== "undefined")
+        document.write(
+          `Error retrieving users: ${JSON.stringify(error)}`
+        );
+      console.error("Error retrieving users", error);
+    });
+  }
+
   render() {
     return (
       <SafeAreaView style={[styles.container]}>
@@ -13,15 +67,6 @@ export default class Leaderboard extends React.Component {
             </Text>
           </View>
           <ScrollView style={{ backgroundColor: "white" }}>
-            <LeaderBoardRow />
-            <LeaderBoardRow />
-            <LeaderBoardRow />
-            <LeaderBoardRow />
-            <LeaderBoardRow />
-            <LeaderBoardRow />
-            <LeaderBoardRow />
-            <LeaderBoardRow />
-            <LeaderBoardRow />
             <LeaderBoardRow />
           </ScrollView>
         </View>
