79209a566fee2d3b73640cf92bb687fd3c9a671a
garcia64 <52061588+voltra40@users.noreply.github.com>
2020-11-05 18:26:26 -0500

    added functionality to display users personalized cash amount in the homescreen #51

--- screens/home/DisplayCash.js
+++ screens/home/DisplayCash.js
@@ -1,13 +1,42 @@
 import * as React from "react";
 import { StyleSheet, Text, View, SafeAreaView } from "react-native";
+import * as SecureStore from "expo-secure-store";
 
 export default class DisplayCash extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      cash: 0,
+    };
+  }
+  async componentDidMount() {
+    await this.getCash();
+  }
+  async getCash() {
+    let cash;
+    let userId;
+    await SecureStore.getItemAsync("userId").then((id) => {
+      userId = JSON.parse(id);
+      console.log("userId: " + userId);
+    });
+    await SecureStore.getItemAsync("cash").then((storedCash) => {
+      cash = storedCash;
+      console.log("cash amount: " + cash);
+    })
+    .catch((error) => {
+      document.write(`Error retrieving cash.`);
+      console.error("Error retrieving cash", error);
+    });
+    this.setState({ cash: cash});
+    console.log("test commas: ");
+  }
+
   render() {
     return (
       <SafeAreaView style={styles.boardContainer}>
         <View>
           <Text style={styles.cashLabel}>Cash:</Text>
-          <Text style={styles.cashValue}> $15,235.53</Text>
+          <Text style={styles.cashValue}> ${this.state.cash} </Text>
         </View>
       </SafeAreaView>
     );
@@ -33,6 +62,6 @@ const styles = StyleSheet.create({
   },
   cashValue: {
     fontSize: 60,
-    color: "#05375a"
+    color: "#05375a",
   },
 });
